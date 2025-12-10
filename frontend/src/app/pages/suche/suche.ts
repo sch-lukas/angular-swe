6cfec52566f8cce7f68b569ed3967b396737a460
staa1011 <aaronstockburger@hotmail.de>
2025-12-10 20:34:13 +0100

    Dateien erstellt

---
+++ frontend/src/app/pages/suche/suche.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suche',
+  imports: [],
+  templateUrl: './suche.html',
+  styleUrl: './suche.css',
+})
+export class Suche {
+
+}
