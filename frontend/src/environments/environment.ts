c38596f5f38191f056931fb64a80a2bb6b80df80
staa1011 <aaronstockburger@hotmail.de>
2025-12-18 13:02:43 +0100

    Tracking auf environment.ts gelöscht

--- frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false,
-    keycloak: {
-        url: '/auth/realms/nest/protocol/openid-connect/token',
-        clientId: 'nest-client',
-        // Dev-only: hier das Secret hinterlegen
-        clientSecret: 'OD58W32Jr1jahDkimToraDryJrsWSxTD',
-    },
-};
